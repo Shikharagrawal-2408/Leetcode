0428b7037e7f6f3f7d53895ccc9f89ea76157f9e
SHIKHAR AGRAWAL <152476746+Shikharagrawal-2408@users.noreply.github.com>
2026-03-10 20:01:23 +0530

    Time: 188 ms (47.92%), Space: 12.6 MB (64.81%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if(n<=2)
+        return 0;
+
+        vector<bool>arr(n,true);
+        arr[0]=arr[1]=false;
+        for(int i=2;i*i<n;i++)
+        {
+            if(arr[i])
+            {
+                for(int j=i*i;j<n;j+=i)
+                arr[j]=false;
+            }
+        }
+
+        return count(arr.begin(),arr.end(),true);
+    }
+};
