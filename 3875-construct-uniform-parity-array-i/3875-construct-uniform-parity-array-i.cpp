da752633f5dee0b142b46cf6419832942325fef0
SHIKHAR AGRAWAL <152476746+Shikharagrawal-2408@users.noreply.github.com>
2026-09-03 00:06:45 +0530

    Time: 0 ms (100%), Space: 30.3 MB (36.43%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        // vector<int>arr;
+
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(nums[i]&1)
+        //     arr.push_back(nums[i]);
+
+        //     if()
+        // }
+
+        return true;
+    }
+};
